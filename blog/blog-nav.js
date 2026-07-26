d58b13b9c8a59b5391def891c8a4ca8944ada40a
danielgoguen <goguend@gmail.com>
2026-07-26 03:32:38 -0300

    Update blog-nav.js

--- blog/blog-nav.js
+++ blog/blog-nav.js
@@ -15,30 +15,33 @@ number.textContent = `${index + 1} / ${blogPosts.length}`;
 if (index > 0) {
     prev.href = blogPosts[index - 1];
 } else {
-    prev.style.visibility = "hidden";
+    prev.style.opacity = "0.35"; // next.style.visibility = "hidden";
+    prev.style.pointerEvents = "none";
 }
 
 // Next button
 if (index < blogPosts.length - 1) {
     next.href = blogPosts[index + 1];
 } else {
-    next.style.visibility = "hidden";
+    next.style.opacity = "0.35"; // next.style.visibility = "hidden";
+    next.style.pointerEvents = "none";
 }
 
 // Display current position
 //bot_number.textContent = `${index + 1} / ${blogPosts.length}`;
-
-// Previous button
+// Bottom Previous button
 if (index > 0) {
     bot_prev.href = blogPosts[index - 1];
 } else {
-    bot_prev.style.visibility = "hidden";
+    bot_prev.style.opacity = "0.35"; // bot_prev.style.visibility = "hidden";
+    bot_prev.style.pointerEvents = "none";
 }
 
-// Next button
+// Bottom Next button
 if (index < blogPosts.length - 1) {
     bot_next.href = blogPosts[index + 1];
 } else {
-    bot_next.style.visibility = "hidden";
+    bot_next.style.opacity = "0.35"; //bot_prev.style.visibility = "hidden";
+    bot_next.style.pointerEvents = "none";
 }
 
